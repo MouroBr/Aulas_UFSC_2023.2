36872eab67ed499896539961dcab26b0ebc989a3
Bruno Brizolla Do Nascimento <104790790+MouroBr@users.noreply.github.com>
2023-09-20 18:09:40 -0300

    lista 10

--- lista_numeros.py
+++ lista_numeros.py
@@ -1,11 +1,18 @@
-from questao01 import lista_aleatoria
+from typing import List
 
-numeros = lista_aleatoria(10)
-cont = 0
+# dada a lista acima eu tenho que devolver uma nova lista com os modulos dos valores
+# passo 1: percorre a lista e verificar se valor é <0
+# caso valor negativo * (-1):
+# cada valor apos ser verificado deve ser realocado em uma nova lista na mesma ordem
+# retornar a tela a lista contendo os módulos
 
-while cont < 10:
+lista_inicial = [8, -8, 45]
+indice = 0
+lista_modulos = []
+while indice < len(lista_inicial):
+    if lista_inicial[indice] >= 0:
+        lista_modulos.append(lista_inicial[indice])
 
-    for numero in numeros:
-        if numero < 0:
-            numero = numero * (-1)
-            lista = [numero]
+    else:
+        valor_negativo = lista_inicial[indice]
+         lista_modulos.append(valor_negativo * -1)
