d9fde91318cc17245d918ec5863a2150a0e0168a
MouroBr <brizolla.bruno@gmail.com>
2023-09-20 19:51:50 -0300

    lista 10

--- lista_numeros.py
+++ lista_numeros.py
@@ -1,18 +1,23 @@
-from typing import List
-
 # dada a lista acima eu tenho que devolver uma nova lista com os modulos dos valores
 # passo 1: percorre a lista e verificar se valor é <0
 # caso valor negativo * (-1):
 # cada valor apos ser verificado deve ser realocado em uma nova lista na mesma ordem
 # retornar a tela a lista contendo os módulos
 
-lista_inicial = [8, -8, 45]
-indice = 0
-lista_modulos = []
-while indice < len(lista_inicial):
-    if lista_inicial[indice] >= 0:
-        lista_modulos.append(lista_inicial[indice])
+def modulo_lista(lista):
+    modulos = []
 
-    else:
-        valor_negativo = lista_inicial[indice]
-         lista_modulos.append(valor_negativo * -1)
+    for numero in lista:
+
+        if numero < 0:
+            modulo = -numero
+        else:
+            modulo = numero
+        modulos.append(modulo)
+
+    return modulos
+
+
+lista_original = [1, -2, 3, -4, 5]
+resultados = modulo_lista(lista_original)
+print(resultados)
