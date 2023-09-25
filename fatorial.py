a3fc4368053902339e3af745cec80d5189bfb510
Bruno Brizolla Do Nascimento <104790790+MouroBr@users.noreply.github.com>
2023-09-25 17:57:29 -0300

    lista 10

--- fatorial.py
+++ fatorial.py
@@ -1,11 +1,14 @@
-numero = int(input())
-fator = 1
-n = 1
-if numero == 0 or numero == 1:
-    print("1")
-else:
-    while n <= numero:
-        fator = fator * n
-        n += 1
-    print(fator)
+def fatorial(numero):
+    fator = 1
+    n = 1
+    if numero == 0 or numero == 1:
+        print("1")
+    else:
+        while n <= numero:
+            fator = fator * n
+            n += 1
+    return fator
 
+
+resultado = fatorial(5)
+print(resultado)
